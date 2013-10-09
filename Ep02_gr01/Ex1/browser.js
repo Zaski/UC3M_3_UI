d0fafc8bc85016718206eb54ee66a979cb2d32f7
Ainiz <scaino@inf.uc3m.es>
2013-10-09 21:07:24 +0200

    Exercise 1 cont
    TODO
    - Refine form style
    - Display logic

---
+++ Ep02_gr01/Ex1/browser.js
@@ -0,0 +1,13 @@
+<!--
+	document.write("You are using " + navigator.appCodeName + " " + navigator.appName + " version " + navigator.appVersion 
+		+ " on " + navigator.platform);
+	document.write("<br><br>");
+			
+	var status;
+	if (navigator.cookieEnabled == true) {
+		status = "ON";
+	} else {
+		status = "OFF";
+	}
+	document.write("Your cookies are " + status + "<br>");			
+-->
