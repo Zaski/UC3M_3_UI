c8f31812e14b4501980df1302963ea88cfd957c3
Zaski <Zaski123@gmail.com>
2013-10-28 18:58:13 +0100

    Semi-final version of Exercise 2. Script and workflow completely stated. Bugs to correct: 1. Submit buttton refreshes the page and erases all changed done by the JS. Using <button onclick=function> instead of <form onsubmit=function> already tried without improvements. 2. <input> borders could not be changed via JS and I don't know why. Lines causing problems are commented ATM.

--- Ep02_gr01/EX 2/script/javascript.js
+++ Ep02_gr01/EX 2/script/javascript.js
@@ -32,44 +32,49 @@ function submitForm() {
 	var emptyFields = checkMandatoryFields();
 	var wrongFields = checkFormat();
 	
+	
 	if (!emptyFields && !wrongFields){
 		sendMail();
-		alert("Form submitted properly");
+		alert("Thank you for fulfilling the form. An email has been sent to the email provided.");
 		return true;
 	}
 	else{
-		if(emptyFields){
-			alert("The following fields are empty: " + emptyFieldsArray);
-		}
-		if(wrongFields){
-			alert("The following fields have a wrong format: " + wrongFieldsArray);
-		}
+		alert("Please, fulfill the form correctly. Fields in red are mandatory and must be filled with a correct format. Fields in orange are not mandatory, but must have a correct format too.");
 		return false;
 	}
 }
 
-/* ------------------------------------ */
+/* --------------- CHECK EMPTY FIELDS --------------------- */
 
 function checkMandatoryFields() {
 	var empty;
 	
 	empty = checkType();
-	empty = checkName();
-	empty = checkMail();
-	empty = checkLocation();
-	empty = checkDescription();
+	if(!empty){
+		empty = checkName();
+	}
+	if(!empty){
+		empty = checkMail();
+	}
+	if(!empty){
+		empty = checkLocation();
+	}
+	if(!empty){
+		empty = checkDescription();
+	}
 	
-	alert("checkMandatoryFields returns: " + empty);
+	alert("[DEBUG] checkMandatoryFields returns: " + empty);
 	
 	return empty;
 }
 
+/*Return true if Type is not selected*/
 function checkType(){
 	if(!type1.checked){
 		if(!type2.checked){
 			if(!type3.checked){
 				emptyFieldsArray.push(" Type");
-				type.style.border="3px solid red";
+				borderRed(type);
 				return true;
 			}
 		}
@@ -79,12 +84,13 @@ function checkType(){
 	}
 }
 
+/*Return true if Username is empty*/
 function checkName(){
-	alert("checkName starts");
+	alert("[DEBUG] checkName starts");
 	if (name.value == null || name.value == ""){
 		emptyFieldsArray.push(" Username");
-		//name.style.border="3px solid red";
-		alert("checkName returns true");
+		//borderRed(name);
+		alert("[DEBUG] checkName returns true");
 		return true;
 	}
 	else{
@@ -92,10 +98,11 @@ function checkName(){
 	}
 }
 
+/*Return true if Mail is empty*/
 function checkMail(){
 	if (mail.value == null || mail.value == ""){
 		emptyFieldsArray.push(" Email");
-		//mail.style.border="3px solid red";
+		//borderRed(mail);
 		return true;
 	}
 	else{
@@ -103,10 +110,11 @@ function checkMail(){
 	}
 }
 
+/*Return true if Location is empty*/
 function checkLocation(){
 	if (location.value == null || location.value == ""){
 		emptyFieldsArray.push(" Location");
-		//location.style.border="3px solid red";
+		//borderRed(location);
 		return true;
 	}
 	else{
@@ -114,10 +122,11 @@ function checkLocation(){
 	}
 }
 
+/*Return true if Description is empty*/
 function checkDescription(){
 	if (description.value == null || description.value == ""){
 		emptyFieldsArray.push(" Description of the problem");
-		//description.style.border="3px solid red";
+		//borderRed(description);
 		return true;
 	}
 	else{
@@ -125,17 +134,77 @@ function checkDescription(){
 	}
 }
 
-/* -------------------------------------- */
+/* ----------------- CHECK WRONGLY FORMATTED FIELDS --------------------- */
 
 function checkFormat() {
-	alert("checkFormat TEST");
-	return true;
+	alert("[DEBUG] checkFormat starts");
+	var wrongFormat;
+	
+	wrongFormat = formatMail();
+	if(!wrongFormat){
+		wrongFormat = formatPhone();
+	}
+	
+	alert("[DEBUG] checkFormat returns: " + wrongFormat);
+	return wrongFormat;
 }
 
-/* -------------------------------------- */
+/*Returns true if format of Mail is wrong. If correct or empty, returns false*/
+function formatMail() {
+
+	var atpos = mail.value.indexOf("@");
+	var dotpos = mail.value.lastIndexOf(".");
+	
+	if(checkMail){
+		return false
+	}
+	else if (atpos<1 || dotpos<atpos+2 || dotpos+2>=mail.value.length){
+		wrongFieldsArray.push(" Mail");
+		//borderRed(mail);
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+/* Returns true if format of Phone is wrong. If correct or empty, returns false*/
+function formatPhone() {
+	var plusPos = phone.value.lastIndexOf("+");
+	
+	if(phone.value == null || phone.value == ""){
+		return false
+	}
+	else if (plusPos != 0){
+		wrongFieldsArray.push(" Phone");
+		//borderOrange(phone);
+		return true
+	}
+	else{
+		return false
+	}
+}
+
+/* ------------------ CHANGE BORDER COLOR WHEN REQUESTED -------------------- */
+
+function borderRed(elemment) {
+	elemment.style.border = "3px solid red";
+}
+
+function borderOrange(elemment) {
+	elemment.style.border = "3px solid orange";
+}
+
+/* --------------------- SEND MAIL TO PROVIDED E-MAIL ----------------- */
 
 function sendMail() {
-	alert("sendMail");
+
+	var link = mail.value
+             + "&subject=" + escape("UI Practice 2 - Group 1")
+             + "&body=" + escape("Thank you for fulfilling our form in Generic Sports.");
+
+    window.location.href = link;
+	
 }
 
 /*--------------------------*/
