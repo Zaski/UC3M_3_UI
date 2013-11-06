45694cd021f85ae7a5550c078038ce805fe5922e
Zaski <Zaski123@gmail.com>
2013-11-06 23:40:37 +0100

    Ex2: game works. To-do: replay button, timer.

--- Ep03_gr01/Ex2/script/cardGame.js
+++ Ep03_gr01/Ex2/script/cardGame.js
@@ -5,25 +5,63 @@ $(document).ready(function(){
 	var numberOfCards;
 	var cardImages;
 	var cardPositions;
-	
+	var numOfClicks;
+	var numOfPairsUnhidden;
 	
 	
 	/* ---------- MAIN ---------- */
-	 $(document).on('click', '#play', function() {
-		console.log("preparing Game...");
+	$(document).on('click', '#play', function() {
 		prepareGame();
-		var image;
-		console.log("assigning values to cards...");
 		assignCards();
-		console.log("hidding cards, showing reverse...");
 		alert("Game ready!");
-		
+		var card1;
+		var card2;
+		var idCard1;
+		var idCard2;
+				
 		$(document).on('click', '.cardReverse', function(){
+			numOfClicks++;
 			var id = $(this).attr('id');
 			var number = id.substring(7, id.length);
-			$('#card' + number).show();
-			$(this).hide();
-			console.log(number);
+			var even = numOfClicks % 2 == 0;
+			console.log(even);
+			
+			if(!even){
+				hideCardShowReverse(number);
+				
+				if(card1 == card2){
+					console.log("previous pair is equal");
+					numOfPairsUnhidden++;
+				}
+				else{
+					console.log("previous pair is different");
+					hidePairOfCards(idCard1, idCard2);
+				}
+				
+				card1 = $('#card' + number).attr('src');
+				idCard1 = number;
+			}
+			else{
+				hideCardShowReverse(number);
+				card2 = $('#card' + number).attr('src');
+				idCard2 = number;
+				
+				if(numOfPairsUnhidden == numberOfCards){
+					alert("Congratulations! You won!");
+				}
+			}
+			
+			
+			/*
+			if(numberOfUnhidden() < 2){
+				hideCardShowReverse(number);
+			}
+			else{
+				console.log("there are 2 unhidden cards");
+				
+			}
+			*/
+			
 		});
 	});
 	
@@ -39,6 +77,8 @@ $(document).ready(function(){
 		cardPositions = new Array();
 		cardImages = new Array();
 		numberOfCards = 12;
+		numOfClicks = 0;
+		numOfPairsUnhidden = 0;
 		
 		fillArrayCardImages();
 		fillArrayCardPositions();
@@ -66,21 +106,39 @@ $(document).ready(function(){
 	}
 	
 	function assignCards(){
+		var image;
 		for(var i=0; i<numberOfCards ; i++){
 		
 			image = cardImages[i];
-			console.log(image);
 			$("#" + cardPositions[i]).attr('src', image);
 			$("#" + cardPositions[(i+numberOfCards)]).attr('src', image);
 		}
 	}
 	
-	function hideCardsShowReverse(){
-		console.log("estoy en hideCardsShowReverse");
-		//test: change attribute 'display' of the first card to 'none'. Expected result: there is 1 card less. Result: there are still 24 cards...
-		$('#card0').attr('display', 'none');
-		$('img.cardReverse').attr('display', 'block');
-		console.log("job done");
+	function hideCardShowReverse(number){
+		$('#card' + number).show();
+		$('#reverse' + number).hide();
+	}
+	
+	/* Return number of unhidden cards*/
+	function numberOfUnhidden(){
+		var status;
+		var unhidden = 0;
+		for(var i=0; i<(numberOfCards*2); i++){
+				status = $('#card' + i).css('display');
+				if(status != "none"){
+					unhidden++;
+				}
+		}
+		return unhidden;
+	}
+	
+	function hidePairOfCards(card1, card2){
+		$('#card' + card1).hide();
+		$('#reverse' + card1).show();
+		
+		$('#card' + card2).hide();
+		$('#reverse' + card2).show();
 	}
 	
 	/* Begin credits to: http://bost.ocks.org/mike/shuffle/ */
